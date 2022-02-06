91c530a4d03b3fffc9eb11d61b608a85af16634c
Avvy <saberwatch@gmail.com>
2022-02-06 15:43:56 -0800

    swiping functionality

--- pages/crew.tsx
+++ pages/crew.tsx
@@ -1,93 +1,142 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import type { NextPage } from "next";
 import Head from "../components/Head";
 import Navigation from "../components/Navigation";
 import styles from "../styles/Home.module.scss";
+import clsx from "clsx";
 import crewmember from "../data.json";
 
 const Crew: NextPage = () => {
-  const [crew, setCrew] = useState<"douglas" | "mark" | "victor" | "anousheh">("douglas");
-  //test
+  const [crew, setCrew] = useState<number>(0);
   const member = crewmember.crew[crew];
+
+  const initial = useRef<number>(0);
+  const limit = useRef<boolean>(false);
 
   return (
     <>
       <Head title={"Space Tourism - Crew"} />
-      <div className={styles.crew}>
+      <div
+        className={styles.crew}
+        onTouchStart={(event) => (initial.current = event.targetTouches[0].clientX)}
+        onTouchMove={(event) => {
+          if (event.targetTouches[0].clientX > initial.current + 60 && crew > 0 && !limit.current) {
+            limit.current = true;
+            setCrew(crew - 1);
+            setTimeout(() => (limit.current = false), 500);
+          } else if (
+            event.targetTouches[0].clientX < initial.current - 60 &&
+            crew < 3 &&
+            !limit.current
+          ) {
+            limit.current = true;
+            setCrew(crew + 1);
+            setTimeout(() => (limit.current = false), 500);
+          }
+        }}
+      >
         <div className={styles.limit}>
           <Navigation />
 
           <main className={styles.main}>
-            <h2 className={styles.section}>
-              <span className={styles.number}>02</span>MEET YOUR CREW
-            </h2>
+            <button
+              className={clsx(styles.arrow, crew === 0 && styles.hide)}
+              onClick={() => {
+                if (crew > 0) {
+                  setCrew(crew - 1);
+                }
+              }}
+            >
+              <svg width="212" height="300">
+                <path stroke="#979797" strokeWidth="3" fill="none" d="M130,50 L70,150 L130,250" />
+              </svg>
+            </button>
 
-            <div className={styles.content}>
-              <div className={styles.imgContainer}>
-                <img src={member.images.png} alt={`${member.role} ${member.name}`} />
-              </div>
+            <div className={styles.notArrow}>
+              <h2 className={styles.section}>
+                <span className={styles.number}>02</span>MEET YOUR CREW
+              </h2>
 
-              <div className={styles.container}>
-                <div className={styles.radio}>
-                  <input
-                    type="radio"
-                    name="crew"
-                    id="douglas"
-                    value="douglas"
-                    checked={crew === "douglas"}
-                    onChange={() => setCrew("douglas")}
-                  />
-                  <label htmlFor="douglas">
-                    <div className={styles.label}></div>
-                  </label>
-
-                  <input
-                    type="radio"
-                    name="crew"
-                    id="mark"
-                    value="mark"
-                    checked={crew === "mark"}
-                    onChange={() => setCrew("mark")}
-                  />
-                  <label htmlFor="mark">
-                    <div className={styles.label}></div>
-                  </label>
-
-                  <input
-                    type="radio"
-                    name="crew"
-                    id="victor"
-                    value="victor"
-                    checked={crew === "victor"}
-                    onChange={() => setCrew("victor")}
-                  />
-                  <label htmlFor="victor">
-                    <div className={styles.label}></div>
-                  </label>
-
-                  <input
-                    type="radio"
-                    name="crew"
-                    id="anousheh"
-                    value="anousheh"
-                    checked={crew === "anousheh"}
-                    onChange={() => setCrew("anousheh")}
-                  />
-                  <label htmlFor="anousheh">
-                    <div className={styles.label}></div>
-                  </label>
+              <div className={styles.content}>
+                <div className={styles.imgContainer}>
+                  <img src={member.images.png} alt={`${member.role} ${member.name}`} />
                 </div>
 
-                <div className={styles.crewDescription}>
-                  <div className={styles.spacing}>
-                    <p className={styles.position}>{member.role}</p>
-                    <p className={styles.name}>{member.name}</p>
+                <div className={styles.container}>
+                  <div className={styles.radio}>
+                    <input
+                      type="radio"
+                      name="crew"
+                      id="douglas"
+                      value="douglas"
+                      checked={crew === 0}
+                      onChange={() => setCrew(0)}
+                    />
+                    <label htmlFor="douglas">
+                      <div className={styles.label}></div>
+                    </label>
+
+                    <input
+                      type="radio"
+                      name="crew"
+                      id="mark"
+                      value="mark"
+                      checked={crew === 1}
+                      onChange={() => setCrew(1)}
+                    />
+                    <label htmlFor="mark">
+                      <div className={styles.label}></div>
+                    </label>
+
+                    <input
+                      type="radio"
+                      name="crew"
+                      id="victor"
+                      value="victor"
+                      checked={crew === 2}
+                      onChange={() => setCrew(2)}
+                    />
+                    <label htmlFor="victor">
+                      <div className={styles.label}></div>
+                    </label>
+
+                    <input
+                      type="radio"
+                      name="crew"
+                      id="anousheh"
+                      value="anousheh"
+                      checked={crew === 3}
+                      onChange={() => setCrew(3)}
+                    />
+                    <label htmlFor="anousheh">
+                      <div className={styles.label}></div>
+                    </label>
                   </div>
 
-                  <p className={styles.bio}>{member.bio}</p>
+                  <div className={styles.crewDescription}>
+                    <div className={styles.spacing}>
+                      <p className={styles.position}>{member.role}</p>
+                      <p className={styles.name}>{member.name}</p>
+                    </div>
+
+                    <p className={styles.bio}>{member.bio}</p>
+                  </div>
                 </div>
               </div>
             </div>
+
+            <button
+              className={clsx(styles.arrow, crew === 3 && styles.hide)}
+              onClick={() => {
+                if (crew < 3) {
+                  setCrew(crew + 1);
+                }
+              }}
+            >
+              <svg width="212" height="300">
+                <path stroke="#979797" strokeWidth="3" fill="none" d="M82,50 L142,150 L82,250" />
+              </svg>
+            </button>
           </main>
         </div>
       </div>
--- pages/technology.tsx
+++ pages/technology.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import type { NextPage } from "next";
 import Head from "../components/Head";
 import Navigation from "../components/Navigation";
@@ -6,13 +6,34 @@ import styles from "../styles/Home.module.scss";
 import sequence from "../data.json";
 
 const Technology: NextPage = () => {
-  const [tech, setTech] = useState<"launch" | "port" | "capsule">("launch");
+  const [tech, setTech] = useState<number>(0);
   const techs = sequence.technology[tech];
+
+  const initial = useRef<number>(0);
+  const limit = useRef<boolean>(false);
 
   return (
     <>
       <Head title={"Space Tourism - Technology"} />
-      <div className={styles.technology}>
+      <div
+        className={styles.technology}
+        onTouchStart={(event) => (initial.current = event.targetTouches[0].clientX)}
+        onTouchMove={(event) => {
+          if (event.targetTouches[0].clientX > initial.current + 60 && tech > 0 && !limit.current) {
+            limit.current = true;
+            setTech(tech - 1);
+            setTimeout(() => (limit.current = false), 500);
+          } else if (
+            event.targetTouches[0].clientX < initial.current - 60 &&
+            tech < 3 &&
+            !limit.current
+          ) {
+            limit.current = true;
+            setTech(tech + 1);
+            setTimeout(() => (limit.current = false), 500);
+          }
+        }}
+      >
         <div className={styles.limit}>
           <Navigation />
 
@@ -24,8 +45,8 @@ const Technology: NextPage = () => {
             <div className={styles.content}>
               <div className={styles.imgContainer}>
                 <img
-                  srcSet={`/img/technology/image-${tech}-landscape.jpg 768w,
-                         /img/technology/image-${tech}-portrait.jpg 515w`}
+                  srcSet={`/img/technology/image-${techs.identifier}-landscape.jpg 768w,
+                         /img/technology/image-${techs.identifier}-portrait.jpg 515w`}
                   sizes="(max-width: 1280px) 1280px,
                        515px"
                 />
@@ -38,8 +59,8 @@ const Technology: NextPage = () => {
                     name="tech"
                     id="launch"
                     value="launch"
-                    checked={tech === "launch"}
-                    onChange={() => setTech("launch")}
+                    checked={tech === 0}
+                    onChange={() => setTech(0)}
                   />
                   <label htmlFor="launch">
                     <p>1</p>
@@ -50,8 +71,8 @@ const Technology: NextPage = () => {
                     name="tech"
                     id="port"
                     value="port"
-                    checked={tech === "port"}
-                    onChange={() => setTech("port")}
+                    checked={tech === 1}
+                    onChange={() => setTech(1)}
                   />
                   <label htmlFor="port">
                     <p>2</p>
@@ -62,8 +83,8 @@ const Technology: NextPage = () => {
                     name="tech"
                     id="capsule"
                     value="capsule"
-                    checked={tech === "capsule"}
-                    onChange={() => setTech("capsule")}
+                    checked={tech === 2}
+                    onChange={() => setTech(2)}
                   />
                   <label htmlFor="capsule">
                     <p>3</p>
